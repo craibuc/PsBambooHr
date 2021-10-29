155c2f1655712c98bb7e7ba72ed57a9c7cc5537c
Craig Buchanan <craig.buchanan@cogniza.com>
2021-10-29 10:10:55 -0500

    initial commit

---
+++ PsBambooHr/Public/New-BambooHrEmployeeFile.ps1
@@ -0,0 +1,152 @@
+<#
+.SYNOPSIS
+Transmits a file to BambooHr and associates with an employee and file category.
+
+.PARAMETER ApiKey
+The API key.
+
+.PARAMETER Subdomain
+The subdomain used to access bamboohr. If you access bamboohr at https://mycompany.bamboohr.com, then the companyDomain is "mycompany"
+
+.PARAMETER Id
+The employee's unique identifier (assigned by Bamboo HR). The employee ID of zero (0) is the employee ID associated with the API key.
+
+.PARAMETER Path
+The path to the file to be transmitted.
+
+.PARAMETER CategoryId
+The category (i.e. the folder) that will contain the file.
+
+.PARAMETER Share
+Share the document with the employee.
+
+.EXAMPLE
+PS> New-BambooHrEmployeeFile -ApiKey '3ee9c09c-c4be-4e0b-9b08-d7df909ae001' -Subdomain 'companyDomain' -Id 1 -Path '~/Desktop/My File.pdf' -CategoryId 28 -Share
+
+Transmits ~/Desktop/My File.pdf to BambooHR, associating it with the empployee with Id 1, placing it in the Uncategoried [28] folder, and sharing it with the employee.
+#>
+function New-BambooHrEmployeeFile
+{
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory)]
+        [string]$ApiKey,
+
+        [Parameter(Mandatory)]
+        [string]$Subdomain,
+
+        [Parameter(Mandatory)]
+        [ValidatePattern('\d')] # numbers only
+        [string]$Id,
+
+        [Parameter(Mandatory)]
+        [string]$Path,
+
+        [Parameter(Mandatory)]
+        [ValidatePattern('\d')] # numbers only
+        [string]$CategoryId,
+
+        [switch]$Share
+    )
+
+    begin {
+        $Headers = @{
+            Accept = 'application/json'
+        }
+
+        # credentials
+        $Password = ConvertTo-SecureString 'Password' -AsPlainText -Force
+        $Credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ApiKey, $Password
+    }
+
+    process {
+
+        if ( (Test-Path -Path $Path) -eq $false )
+        {
+            throw [System.IO.FileNotFoundException]::new("File not found",$Path)
+        }
+
+        # $boundary = [System.Guid]::NewGuid().ToString();
+        # $LF = "`r`n";
+
+        #
+        # process file
+        #
+
+        # $Item = Get-Item -Path $Path
+
+        # $fileBytes = [System.IO.File]::ReadAllBytes($Path);
+        # $fileEnc = [System.Text.Encoding]::GetEncoding('UTF-8').GetString($fileBytes);
+
+        # $fileTemplate =
+        # "--$boundary
+        # Content-Disposition: form-data; name=`"`"; filename=`"{0}`"
+        # Content-Type: application/octet-stream
+        
+        # {1}" -f $Item.Name, $fileEnc
+        
+        # $bodyLines = ( 
+        #     "--$boundary",
+        #     "Content-Disposition: form-data; name=`"file`"; filename=`"$Item.Name`"",
+        #     "Content-Type: application/octet-stream$LF",
+        #     $fileEnc,
+        #     "--$boundary--$LF" 
+        # ) -join $LF
+
+        #
+        # other attributes
+        #
+        
+        # $template = 
+        # "--$boundary
+        # Content-Disposition: form-data; name=`"{0}`"
+
+        # {1}"
+
+                # Invoke-RestMethod -Uri $Uri -Method Post -ContentType "multipart/form-data; boundary=`"$boundary`"" -Body $bodyLines
+
+        $Item = Get-Item -Path $Path
+
+        $Form = @{
+            file = $Item
+            fileName = $Item.Name
+            category = $CategoryId
+            share = $Share.IsPresent ? 'yes' : 'no'
+        }
+    
+        $Uri = "https://api.bamboohr.com/api/gateway.php/$Subdomain/v1/employees/$Id/files"
+        Write-Debug "Uri: $Uri"
+        
+        # $FileHeader = [System.Net.Http.Headers.ContentDispositionHeaderValue]::new('form-data')
+        # $FileHeader.Name = $FieldName
+        # $FileHeader.FileName = Split-Path -Leaf $Path
+        # $FileHeader.Size = $Item.Length
+
+        # $ResolvedPath = Resolve-Path $Path
+
+        # $FileStream = [System.IO.FileStream]::new($ResolvedPath, [System.IO.FileMode]::Open)
+        # $FileContent = [System.Net.Http.StreamContent]::new($FileStream)
+
+        # $FileContent.Headers.ContentDisposition = $FileHeader
+        # $FileContent.Headers.ContentType = [System.Net.Http.Headers.MediaTypeHeaderValue]::Parse('application/octet-stream')
+        
+        # $MultipartContent = [System.Net.Http.MultipartFormDataContent]::new()
+        # $MultipartContent.Add($FileContent)
+        
+        # $MultipartContent.Add($CategoryId, 'category')
+
+        try
+        {
+            # $Response = Invoke-WebRequest -Uri $Uri -Method Post -Body $MultipartContent -Headers $Headers -Credential $Credentials -UseBasicParsing
+            $Response = Invoke-WebRequest -Proxy 'http://localhost:8080' -Uri $Uri -Method Post -Form $Form -Headers $Headers -Credential $Credentials -UseBasicParsing
+            $Response
+        }
+        catch
+        {
+            Write-Error -Message $_.Exception.Message
+        }
+
+    }
+
+    end {}
+}
---
+++ Tests/Public/New-BambooHrEmployeeFile.Tests.ps1
@@ -0,0 +1,131 @@
+BeforeAll {
+
+    # /PsBambooHr
+    $ProjectDirectory = Split-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -Parent
+
+    # /PsBambooHr/PsBambooHr/Public
+    $PublicPath = Join-Path $ProjectDirectory "/PsBambooHr/Public/"
+
+    # /PsBambooHr/Tests/Fixtures/
+    $FixturesDirectory = Join-Path $ProjectDirectory "/Tests/Fixtures/"
+
+    # New-BambooHrEmployeeFile.ps1
+    $SUT = (Split-Path -Leaf $PSCommandPath) -replace '\.Tests\.', '.'
+
+    . (Join-Path $PublicPath $SUT)
+
+}
+
+Describe "New-BambooHrEmployeeFile" -Tag 'unit' {
+
+    Context "Parameter validation" {
+
+        BeforeAll {
+            $Command = Get-Command 'New-BambooHrEmployeeFile'
+        } 
+
+        $Parameters = @(
+            @{ParameterName='ApiKey';Type=[string]; Mandatory=$true}
+            @{ParameterName='Subdomain';Type=[string]; Mandatory=$true}
+            @{ParameterName='ID';Type=[string]; Mandatory=$true}
+            @{ParameterName='Path';Type=[string]; Mandatory=$true}
+            @{ParameterName='CategoryId';Type=[string]; Mandatory=$true}
+            @{ParameterName='Share';Type=[switch]; Mandatory=$false}
+        )
+
+        Context "Type" {
+            It '<ParameterName> mandatory is a <Type>' -TestCases $Parameters {
+                param($ParameterName, $Type)
+                $Command | Should -HaveParameter $ParameterName -Type $Type
+            }    
+        }
+
+        Context "Mandatory" {
+            it '<ParameterName> mandatory is <Mandatory>' -TestCases $Parameters {
+                param($ParameterName, $Type, $Mandatory)
+              
+                if ($Mandatory) { $Command | Should -HaveParameter $ParameterName -Mandatory }
+                else { $Command | Should -HaveParameter $ParameterName -Not -Mandatory }
+            }    
+        }
+
+    } # /Context
+
+    Context "Usage" {
+
+        BeforeAll {
+            # arrange
+            $Authentication = @{
+                ApiKey = '2134d8d5-d1b4-4a1d-89ac-f44a96514bb5'
+                Subdomain = 'subdomain'    
+            }
+
+            $Path = Join-Path $TestDrive "MyFile.txt"
+            New-Item -ItemType File -Path $Path
+
+            $Splat = @{
+                Id = 1
+                Path = $Path
+                CategoryId = 28
+                Share = $true    
+            }
+
+            Mock Invoke-WebRequest {
+                $Response = New-MockObject -Type  Microsoft.PowerShell.Commands.BasicHtmlWebResponseObject
+                $Response | Add-Member -Type NoteProperty -Name 'Content' -Value $null -Force
+                $Response
+            }
+
+        }
+
+        BeforeEach {
+            # act
+            New-BambooHrEmployeeFile @Authentication @Splat
+        }
+
+        Context "Request" {
+
+            It "uses the correct Uri" {
+                # assert
+                Assert-MockCalled Invoke-WebRequest -ParameterFilter { 
+                    $Uri -eq "https://api.bamboohr.com/api/gateway.php/$( $Authentication.Subdomain )/v1/employees/$( $Splat.Id )/files"
+                }
+            }
+    
+            It "uses the correct Method" {    
+                # assert
+                Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Method -eq 'Post' }
+            }
+        
+            It "uses the correct Accept" {
+                # assert
+                Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Headers.Accept -eq 'application/json' }
+            }
+    
+            It "uses the correct Credential" {
+                # arrange
+                $BasicCredential = [pscredential]::new($Authentication.ApiKey, ('Password' | ConvertTo-SecureString -AsPlainText -Force) )
+
+                # assert
+                Assert-MockCalled Invoke-WebRequest -ParameterFilter { 
+                    $UseBasicParsing -eq $true -and
+                    $Credential.UserName -eq $BasicCredential.UserName -and
+                    ($Credential.Password | ConvertFrom-SecureString -AsPlainText) -eq ($BasicCredential.Password | ConvertFrom-SecureString -AsPlainText)
+                }
+            }
+
+            It "uses the correct Form" {
+                # assert
+                Assert-MockCalled Invoke-WebRequest -ParameterFilter {
+                    # $Form.file -eq $Path -and
+                    $Form.fileName -eq (Split-Path $Splat.Path -Leaf) -and
+                    $Form.category -eq $Splat.CategoryId -and
+                    $Form.share -eq ($Splat.Share ? 'yes' : 'no')        
+                }
+            }
+
+        } # /Context
+
+    } # /Context
+
+} # /Describe
