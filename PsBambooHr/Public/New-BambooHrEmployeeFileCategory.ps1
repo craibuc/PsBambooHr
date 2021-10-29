5bb6ab8ba5939cd3f10f454fd705d33c767209b1
Craig Buchanan <craig.buchanan@cogniza.com>
2021-10-29 08:53:14 -0500

    initial commit

---
+++ PsBambooHr/Public/New-BambooHrEmployeeFileCategory.ps1
@@ -0,0 +1,69 @@
+<#
+.SYNOPSIS
+Add an employee file category.
+
+.PARAMETER ApiKey
+The API key.
+
+.PARAMETER Subdomain
+The subdomain used to access bamboohr. If you access bamboohr at https://mycompany.bamboohr.com, then the companyDomain is "mycompany"
+
+.PARAMETER Name
+An array of categories to add.
+
+.EXAMPLE
+
+# PS> New-BambooHrEmployeeFileCategory -ApiKey '3ee9c09c-c4be-4e0b-9b08-d7df909ae001' -Subdomain 'companyDomain' -Name 'Category I','Category II'
+
+.LINK
+https://documentation.bamboohr.com/reference#add-employee-file-category-1
+
+#>
+function New-BambooHrEmployeeFileCategory
+{
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$ApiKey,
+
+        [Parameter(Mandatory)]
+        [string]$Subdomain,
+
+        [Parameter(Mandatory)]
+        [string[]]$Name
+    )
+
+    begin {
+        # headers
+        $Headers = @{ Accept = 'application/json' }
+
+        # uri
+        $Uri = "https://api.bamboohr.com/api/gateway.php/$Subdomain/v1/employees/files/categories"
+        Write-Debug "Uri: $Uri"
+
+        # credentials
+        $Password = ConvertTo-SecureString 'Password' -AsPlainText -Force
+        $Credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ApiKey, $Password
+    }
+
+    process {
+
+        try {
+            $Response = Invoke-WebRequest -Uri $Uri -Method Post -Body ($Name | ConvertTo-Json) -Headers $Headers -Credential $Credentials -UseBasicParsing
+            # switch ($Response.StatusCode) {
+            #     condition {  }
+            #     Default {}
+            # }
+            # if ($Response -ne 201) {}
+            $Response.Content | ConvertFrom-Json
+        }
+        catch {
+            Write-Error -Message $_.Exception.Message
+        }
+
+    }
+
+    end {}
+
+}
---
+++ Tests/Public/New-BambooHrEmployeeFileCategory.Tests.ps1
@@ -0,0 +1,112 @@
+BeforeAll {
+    
+    # /PsBambooHr
+    $ProjectDirectory = Split-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -Parent
+
+    # /PsBambooHr/PsBambooHr/Public
+    $PublicPath = Join-Path $ProjectDirectory "/PsBambooHr/Public/"
+
+    # /PsBambooHr/Tests/Fixtures/
+    $FixturesDirectory = Join-Path $ProjectDirectory "/Tests/Fixtures/"
+
+    # New-BambooHrEmployeeFileCategory.ps1
+    $SUT = (Split-Path -Leaf $PSCommandPath) -replace '\.Tests\.', '.'
+
+    . (Join-Path $PublicPath $SUT)
+
+}
+
+Describe "New-BambooHrEmployeeFileCategory" -Tag 'unit' {
+
+    Context "Parameter validation" {
+
+        BeforeAll {
+            $Command = Get-Command 'New-BambooHrEmployeeFileCategory'
+        } 
+
+        $Parameters = @(
+            @{ParameterName='ApiKey';Type=[string]; Mandatory=$true}
+            @{ParameterName='Subdomain';Type=[string]; Mandatory=$true}
+            @{ParameterName='Name';Type=[string[]]; Mandatory=$true}
+        )
+
+        Context "Type" {
+            It '<ParameterName> mandatory is a <Type>' -TestCases $Parameters {
+                param($ParameterName, $Type)
+                $Command | Should -HaveParameter $ParameterName -Type $Type
+            }    
+        }
+
+        Context "Mandatory" {
+            it '<ParameterName> mandatory is <Mandatory>' -TestCases $Parameters {
+                param($ParameterName, $Type, $Mandatory)
+              
+                if ($Mandatory) { $Command | Should -HaveParameter $ParameterName -Mandatory }
+                else { $Command | Should -HaveParameter $ParameterName -Not -Mandatory }
+            }    
+        }
+
+    }
+
+    Context "Usage" {
+
+        BeforeAll {
+            # arrange
+            $ApiKey = '2134d8d5-d1b4-4a1d-89ac-f44a96514bb5'
+            $Password = ConvertTo-SecureString 'Password' -AsPlainText -Force
+            $Subdomain = 'subdomain'
+            $Name = 'Foo','Bar'
+
+            Mock Invoke-WebRequest {
+                $Response = New-MockObject -Type  Microsoft.PowerShell.Commands.BasicHtmlWebResponseObject
+                $Response | Add-Member -Type NoteProperty -Name 'Content' -Value '' -Force
+                $Response
+            }
+        }
+
+        Context "Request" {
+
+            BeforeEach {
+                # act
+                New-BambooHrEmployeeFileCategory -ApiKey $ApiKey -Subdomain $Subdomain -Name $Name
+            }
+    
+            It "uses the correct Uri" {
+                # assert
+                Assert-MockCalled Invoke-WebRequest -ParameterFilter { 
+                    Write-Debug "Uri: $Uri"
+                    $Uri -eq "https://api.bamboohr.com/api/gateway.php/$Subdomain/v1/employees/files/categories"
+                }
+            }
+    
+            It "uses the correct Method" {    
+                # assert
+                Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Method -eq 'Post' }
+            }
+        
+            It "uses the correct Accept" {
+                # assert
+                Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Headers.Accept -eq 'application/json' }
+            }
+    
+            It "uses the correct Credential" {
+                # arrange
+                $BasicCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ApiKey, $Password
+
+                # assert
+                Assert-MockCalled Invoke-WebRequest -ParameterFilter { 
+                    $Credential.UserName -eq $BasicCredential.UserName -and
+                    ($Credential.Password | ConvertFrom-SecureString -AsPlainText) -eq ($BasicCredential.Password | ConvertFrom-SecureString -AsPlainText) -and
+                    $UseBasicParsing -eq $true
+                }
+            }
+
+            It "uses the correct Body" {
+                # assert
+                Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Body -eq ( $Name | ConvertTo-Json ) }
+            }
+
+        }
+    }
+
+}
