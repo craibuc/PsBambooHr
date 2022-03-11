d5835405994a9bda6f0926c94d9bd2de52b99200
Craig Buchanan <craig.buchanan@cogniza.com>
2022-03-11 08:37:40 -0600

    adding help text

--- PsBambooHr/Public/Set-BambooHrEmployeeTableRow.ps1
+++ PsBambooHr/Public/Set-BambooHrEmployeeTableRow.ps1
@@ -1,3 +1,38 @@
+<#
+.SYNOPSIS
+Updates the row for the specified table.
+
+.DESCRIPTION
+Updates the data for a given employee, table, and row combination.
+
+.PARAMETER ApiKey
+The API key.
+
+.PARAMETER Subdomain
+The subdomain used to access bamboohr.
+
+.PARAMETER EmployeeId
+The employee's unique identifier.
+
+.PARAMETER TableName
+The name of the table.
+
+.PARAMETER RowId
+The row's unique identifier.
+
+.PARAMETER Data
+A [pscustomobject] represenation of the row's data.
+
+.EXAMPLE
+[pscustomobject]@{
+    Field0 = 1
+    Field1 = 'abc'
+} | Set-BambooHrEmployeeTableRow -ApiKey '3ee9c09c-c4be-4e0b-9b08-d7df909ae001' -Subdomain 'companyDomain' -EmployeeId 1 -TableName 'MyTable' -RowId 1000
+
+.LINK
+https://documentation.bamboohr.com/reference/update-employee-table-row-1
+#>
+
 function Set-BambooHrEmployeeTableRow 
 {
     [CmdletBinding(SupportsShouldProcess)]
@@ -34,6 +69,9 @@ function Set-BambooHrEmployeeTableRow
     process {
 
         try {
+
+            $Data.Remove('employeeId') # supplying the employeeId will cause the POST to fail
+            $Data.Remove('id') # supplying the row id will cause the POST to fail
 
             $Body = $Data | ConvertTo-Json
 
